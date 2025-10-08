8e261800d845a6db7a5a6dcf4d52a75b0d735fc9
ThuyenTruongHau <haurollzabet2306@gmail.com>
2025-10-08 11:36:53 +0700

    deploy/fix_cor

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -172,9 +172,28 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20,
 }
 
-
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 # JWT settings
 from datetime import timedelta
 SIMPLE_JWT = {
